b8e0271003c455b69bc1ad1a3dbc17bde53d3d7d
pushadan <35050497+pushadan@users.noreply.github.com>
2024-04-25 17:31:20 -0700

    Created dotnet project directory framework

---
+++ ShapeTracker.Solution/ShapeTracker/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program{
+  static void Main(){
+    // entry point
+    Console.WriteLine("~~~~~~~\n\n\n");
+
+
+    Console.WriteLine("Everything is working 👌");
+
+    
+    Console.WriteLine("\n\n\n~~~~~~~");
+
+  }
+}
