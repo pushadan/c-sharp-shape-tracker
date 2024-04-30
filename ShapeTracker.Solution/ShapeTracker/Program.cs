a7ed386da112639c82c0a0da65ff777d1deb2800
pushadan <35050497+pushadan@users.noreply.github.com>
2024-04-29 20:26:51 -0700

    Adjust folder structure

--- ShapeTracker.Solution/ShapeTracker/Program.cs
+++ ShapeTracker.Solution/ShapeTracker/Program.cs
@@ -3,14 +3,23 @@ using ShapeTracker.Models;
 
 
 namespace ShapeTracker{ 
+
   class Program{
+    
+    static readonly string[] UIshapeToEvaluate = new string[]{
+      "Triangle Type",
+      "Rectangle"
+    };
+
     static void Main(){
       // entry point
       Console.WriteLine("~~~~~~~");
       Console.WriteLine("Everything is working 👌");
 
-      Triangle newTriangle = new Triangle();
-      Console.WriteLine(newTriangle); //prints object type
+      // Triangle newTriangle = new Triangle(5, 19, 39);
+      Console.WriteLine("Welcome to the Shape Tracker!");
+      Console.WriteLine("What action would you like to take?");
+
 
       
       Console.WriteLine("\n\n\n~~~~~~~");
